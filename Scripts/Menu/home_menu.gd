2fc24a2ef204ac6b9bd7cef77639023a91bb675d
R-Reeve <168000783+R-Reeve@users.noreply.github.com>
2024-12-03 08:26:39 +0700

    commit coba-coba part2

--- Scripts/Menu/home_menu.gd
+++ Scripts/Menu/home_menu.gd
@@ -9,7 +9,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu_Scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://Scenespreload/Menu_Scenes/level_select.tscn")
 	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
